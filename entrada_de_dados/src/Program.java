ba65a9ca373815e9593d4320ef3da58da73e5872
Johnny Matheus <alunotioficial@gmail.com>
2026-02-24 17:46:05 -0300

    revisando

--- entrada_de_dados/src/Program.java
+++ entrada_de_dados/src/Program.java
@@ -1,10 +1,15 @@
+import java.util.Locale;
+
 public class Program {
 	public static void main(String[] args) {
-		
+
 		double x = 10.35784;
 		System.out.println(x);
-		System.out.printf("%.2f%n",x);
-		System.out.printf("%.4f%n",x);
-		
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f%n", x);
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.4f%n", x);
+		System.out.printf("Resultado = %.4f metros%n", x);
+		System.out.println("Resultado =" + x + " metros");
 	}
 }
